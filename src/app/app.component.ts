7baf33593023d99954a42c1f9967c73ae6656edf
Tristan Salinas <salinas.tristan@gmail.com>
2025-06-08 15:14:45 +0200

    08/06 prototype version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { ChatService } from './services/chat.service';
 
@@ -15,6 +15,7 @@ import { ChatService } from './services/chat.service';
 export class AppComponent implements OnInit {
   readonly authService = inject(AuthService);
   readonly chatService = inject(ChatService);
+  readonly router = inject(Router);
   ngOnInit() {
     this.authService.fetchCurrentUser();
     this.chatService.init();
